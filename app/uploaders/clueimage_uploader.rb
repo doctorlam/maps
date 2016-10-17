37874b5a7d6970f1def6b8209b311fa31342d34e
Chris <chris.lam.unt@gmail.com>
2016-10-17 14:54:13 -0500

    set up amazon s3 again

--- app/uploaders/clueimage_uploader.rb
+++ app/uploaders/clueimage_uploader.rb
@@ -1,15 +1,15 @@
 class ClueimageUploader < CarrierWave::Uploader::Base
  include CarrierWave::RMagick
 
-  storage :fog
-process :resize_to_limit => [500, 0]
+  	storage :fog
+	process :resize_to_limit => [500, 0]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :thumb do
-process :resize_to_limit => [500, 0]
+  	version :thumb do
+	process :resize_to_limit => [500, 0]
   end
 
 end
